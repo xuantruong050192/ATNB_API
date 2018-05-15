3783c5a7dbbd1faea1856f29538b750da4d4cbc2
NguyenTruongDev <nguyentruongdeveloper@gmail.com>
2018-05-16 05:57:19 +0700

    eidt controller book

--- ATNB_API/BookManagementAPI/BookManagementAPI/Controllers/BookController.cs
+++ ATNB_API/BookManagementAPI/BookManagementAPI/Controllers/BookController.cs
@@ -7,6 +7,7 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using System.Data.Entity;
+using System.IO;
 
 namespace BookManagementAPI.Controllers
 {
@@ -22,11 +23,18 @@ namespace BookManagementAPI.Controllers
 
                 using (var _unitOfWork = new UnitOfWork())
                 {
-                    
+
                     obj = new
                     {
                         StatusCode = 200,
-                        data = _unitOfWork._context.Books.Where(x => (x.IsActive == true && x.Title.Contains(searchname)) || (x.IsActive == true && searchname == "0")).Include(x=>x.Category).Include(x=>x.Author).Include(x=>x.Publisher).OrderByDescending(x => x.CreateDay).Skip(skip * pagesize).Take(pagesize).ToList(),
+                        data = _unitOfWork._context.Books.Where(x => (x.IsActive == true && x.Title.Contains(searchname)) || (x.IsActive == true && searchname == "0"))
+                        .Include(x => x.Category)
+                        .Include(x => x.Author)
+                        .Include(x => x.Publisher)
+                        .Include(x => x.BookStatu)
+                        .OrderByDescending(x => x.CreateDay)
+                        .Skip(skip * pagesize).Take(pagesize)
+                        .ToList(),
                         total = _unitOfWork._context.Books.Count(x => (x.IsActive == true && x.Title.Contains(searchname)) || (x.IsActive == true && searchname == "0"))
                     };
 
@@ -45,7 +53,7 @@ namespace BookManagementAPI.Controllers
 
 
         }
-        
+
         [HttpGet]
         [Route("api/Book/gettotalrecord")]
         public HttpResponseMessage GetTotalRecord()
@@ -69,30 +77,56 @@ namespace BookManagementAPI.Controllers
 
         }
         [HttpPost]
-        public HttpResponseMessage AddBook([FromBody] Book p_Book)
+        public HttpResponseMessage AddBook()
         {
-            object obj;
-
-            try
+            object obj = new
             {
-                using (var _unitOfWork = new UnitOfWork())
+
+            };
+            int iUploadedCnt = 0;
+
+            // DEFINE THE PATH WHERE WE WANT TO SAVE THE FILES.
+            string sPath = "";
+            sPath = System.Web.Hosting.HostingEnvironment.MapPath("~/Images/");
+            System.Web.HttpFileCollection hfc = System.Web.HttpContext.Current.Request.Files;
+            for (int iCnt = 0; iCnt <= hfc.Count - 1; iCnt++)
+            {
+                System.Web.HttpPostedFile hpf = hfc[iCnt];
+
+                if (hpf.ContentLength > 0)
                 {
-                    p_Book.IsActive = true;
-                    p_Book.CreateDay = DateTime.Now;
-
-
-                    _unitOfWork.BookRepository.Insert(p_Book);
-                    _unitOfWork.Save();
-                    obj = new { StatusCode = 200, data = p_Book };
+                    // CHECK IF THE SELECTED FILE(S) ALREADY EXISTS IN FOLDER. (AVOID DUPLICATE)
+                    if (!File.Exists(sPath + Path.GetFileName(hpf.FileName)))
+                    {
+                        // SAVE THE FILES IN THE FOLDER.
+                        hpf.SaveAs(sPath + Path.GetFileName(hpf.FileName));
+                        iUploadedCnt = iUploadedCnt + 1;
+                    }
                 }
-
-
-
             }
-            catch (Exception e)
-            {
-                obj = new { StatusCode = 500, data = new Book() };
-            }
+
+          
+
+            //try
+            //{
+            //    using (var _unitOfWork = new UnitOfWork())
+            //    {
+            //        p_Book.IsActive = true;
+            //        p_Book.CreateDay = DateTime.Now;
+
+
+            //        _unitOfWork.BookRepository.Insert(p_Book);
+            //        _unitOfWork.Save();
+            //        obj = new { StatusCode = 200, data = p_Book };
+            //    }
+
+
+
+            //}
+            //catch (Exception e)
+            //{
+            //    obj = new { StatusCode = 500, data = new Book() };
+            //}
             return Request.CreateResponse(obj);
         }
         [HttpPut]
